ea510db8a66d547f80c7a6f9a2fe181384edd184
Ace Nassri <anassri@google.com>
2018-01-17 10:57:14 -0800

    Second draft of autodeploy function

--- functions/autodeploy/index.js
+++ functions/autodeploy/index.js
@@ -14,16 +14,44 @@
  */
 
 // [START functions_autodeploy]
-const childProcess = require(`child_process`);
+const google = require('googleapis');
 
 // TODO(developer) configure these settings
 const PROJECT_ID = process.env.GCLOUD_PROJECT;
+const GCF_REGION = '[YOUR_GCF_REGION]';
 const REPO_NAME = `[YOUR_REPO_NAME]`;
-const SOURCE_PATH = `function/directory`; // Must be a directory, not a file
+const SOURCE_PATH = `functions/directory/`; // Must be a directory, not a file
 const FUNCTION_NAME = `[YOUR_FUNCTION_NAME]`;
+const BRANCH_NAME = `master`; // Change this to deploy from a different branch
 
 // Create Cloud Source Repository URL to the directory to deploy from
-const fileUrl = `https://source.developers.google.com/projects/${PROJECT_ID}/repos/${REPO_NAME}/moveable-aliases/master/paths/${SOURCE_PATH}`;
+const folderUrl = `https://source.developers.google.com/projects/${PROJECT_ID}/repos/${REPO_NAME}/moveable-aliases/${BRANCH_NAME}/paths/${SOURCE_PATH}`;
+
+// Helper function to create/get an OAuth 2.0 client instance
+let _client = null;
+function getOAuthClient () {
+  return new Promise((resolve, reject) => {
+    if (!_client) {
+      google.auth.getApplicationDefault((err, authClient) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+          authClient = authClient.createScoped(['https://www.googleapis.com/auth/cloud-platform']);
+        }
+        _client = google.cloudfunctions({
+          version: 'v1beta2',
+          auth: authClient
+        });
+        resolve(_client);
+      });
+    } else {
+      // Use existing client instance
+      resolve(_client);
+    }
+  });
+}
 
 /**
  * HTTP Cloud Function that, when triggered, deploys functions from a Cloud
@@ -33,16 +61,51 @@ const fileUrl = `https://source.developers.google.com/projects/${PROJECT_ID}/rep
  * @param {Object} res Cloud Function response context.
  */
 exports.deployToGCF = (req, res) => {
-  try {
-    // Deploy the function
-    childProcess.execSync(`gcloud beta functions deploy ${FUNCTION_NAME} --source ${fileUrl} --trigger-http`);
+  // Create configuration objects
+  const location = `projects/${PROJECT_ID}/locations/${GCF_REGION}`;
+  const resource = {
+    sourceRepositoryUrl: folderUrl,
+    name: `${location}/functions/${FUNCTION_NAME}`,
+    httpsTrigger: {}
+  };
 
-    // Deployment successful
-    res.send(`Function ${FUNCTION_NAME} was deployed!`);
-  } catch (err) {
-    // Handle deployment error
-    console.error(err);
-    res.status(500).send(`Function ${FUNCTION_NAME} deployment failed.`);
-  }
+  return getOAuthClient()
+    .then((oauthClient) => {
+      return oauthClient.projects.locations.functions;
+    })
+    .then((functionsClient) => {
+      // Create the Cloud Function if it doesn't exist
+      return new Promise((resolve, reject) => {
+        functionsClient.create({ resource, location }, (err) => {
+          if (err &&
+              err.errors &&
+              err.errors[0] &&
+              err.errors[0].reason === 'alreadyExists') {
+            return resolve(functionsClient);
+          } else {
+            return reject(err);
+          }
+        });
+      });
+    }).then((functionsClient) => {
+      // Update the Cloud Function if it does exist
+      return new Promise((resolve, reject) => {
+        functionsClient.update({ resource, name: resource.name }, (err) => {
+          // Report status of update operation
+          if (err) {
+            return reject(err);
+          }
+          return resolve();
+        });
+      });
+    }).then(() => {
+      // Report success
+      res.send(`Function ${FUNCTION_NAME} redeployed successfully!`);
+    })
+    .catch((err) => {
+      // Handle deployment error
+      console.error(err);
+      res.status(500).send(`Function ${FUNCTION_NAME} deployment failed.`);
+    });
 };
 // [END functions_autodeploy]
