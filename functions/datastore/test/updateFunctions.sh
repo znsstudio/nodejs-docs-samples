58a2271b84be98f955f8839a3c423044f4d56e9d
Ace Nassri <anassri@google.com>
2018-03-30 13:58:51 -0700

    Try fixing circle (again)

--- functions/datastore/test/updateFunctions.sh
+++ functions/datastore/test/updateFunctions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Shell script to emulate/deploy all Cloud Functions in the file
 
-${FUNCTIONS_CMD} deploy set --trigger-http
+${FUNCTIONS_CMD} deploy set --trigger-http -p ${GCLOUD_PROJECT}
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy get --trigger-http
+${FUNCTIONS_CMD} deploy get --trigger-http -p ${GCLOUD_PROJECT}
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy del --trigger-http
+${FUNCTIONS_CMD} deploy del --trigger-http -p ${GCLOUD_PROJECT}
